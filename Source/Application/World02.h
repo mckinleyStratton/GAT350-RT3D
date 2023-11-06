8ab69b8fc70ad7f048381176c52b1d89a9831530
mckinleyStratton <mstratton@student.neumont.edu>
2023-11-06 11:18:12 -0700

    cahnged a bunch of vector 2 to glm::vec2 in class 11/6 11:18 b&r

--- Source/Application/World02.h
+++ Source/Application/World02.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Framework/World.h"
-#include "Core/Math/Vector2.h"
+#include <glm/glm/glm.hpp>
 #include "Renderer/Renderer.h"
 #include <vector>
 
@@ -17,8 +17,8 @@ namespace nc
 	private:
 		float m_angle = 0;
 		float m_time;
-		vec2 m_position;
-		std::vector<vec2> m_positions;
+		glm::vec2 m_position;
+		std::vector<glm::vec2> m_positions;
 
 		GLuint m_vao; //Vertex Array Object
 	};
