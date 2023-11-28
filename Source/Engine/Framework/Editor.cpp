ed37932a052af2e6631399a5959c114289075190
mckinleyStratton <mstratton@student.neumont.edu>
2023-11-27 22:27:19 -0700

    Gui filter assignment done

--- Source/Engine/Framework/Editor.cpp
+++ Source/Engine/Framework/Editor.cpp
@@ -25,6 +25,48 @@ namespace nc
 			}
 		}
 
+		// textures
+		if (ImGui::CollapsingHeader("Textures"))
+		{
+			auto resources = GET_RESOURCES(Texture);
+			for (auto& resource : resources)
+			{
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+
+		// models
+		if (ImGui::CollapsingHeader("Models"))
+		{
+			auto resources = GET_RESOURCES(Model);
+			for (auto& resource : resources)
+			{
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+
+		// shaders
+		if (ImGui::CollapsingHeader("Shaders"))
+		{
+			auto resources = GET_RESOURCES(Shader);
+			for (auto& resource : resources)
+			{
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+
+		// materials
+		if (ImGui::CollapsingHeader("Materials"))
+		{
+			auto resources = GET_RESOURCES(Material);
+			for (auto& resource : resources)
+			{
+				if (ImGui::Selectable(resource->name.c_str(), resource.get() == m_selected)) m_selected = resource.get();
+			}
+		}
+
+
+
 
 		ImGui::End();
 
